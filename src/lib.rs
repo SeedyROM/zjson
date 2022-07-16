f280364030797733d844a73ddd42326a62183128
Zack Kollar <rallokkcaz@gmail.com>
2022-07-16 04:22:30 -0700

    :hammer: Work on docs, doc focused testing and actual coverage of the code I'm writing

--- src/lib.rs
+++ src/lib.rs
@@ -2,35 +2,63 @@
 
 #![deny(missing_docs)]
 
-use nom::{bytes::complete::tag, error::context, sequence::delimited};
+use nom::{branch::alt, bytes::complete::tag, error::context, sequence::delimited};
 
 /// The type of token returned from the tokenizer.
-#[derive(Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[derive(Debug, Eq, PartialEq)]
 pub enum Token<'a> {
     /// A single/double quoted string delimited value.
     String(&'a str),
 }
 
 /// Parse a single quoted string.
+///
+/// ## Example:
 /// ```
 /// # use zjson::*;
 /// let (_, string) = parse_single_quoted_string("'hello'").unwrap();
 /// assert_eq!(string, Token::String("hello"));
 /// ```
 pub fn parse_single_quoted_string(input: &str) -> nom::IResult<&str, Token> {
-    context("string", delimited(tag("'"), tag("hello"), tag("'")))(input)
-        .map(|(next_input, res)| (next_input, Token::String(res.into())))
+    context(
+        "single_quoted_string",
+        delimited(tag("'"), tag("hello"), tag("'")),
+    )(input)
+    .map(|(next_input, res)| (next_input, Token::String(res.into())))
 }
 
 /// Parse a double quoted string.
+///
+/// ## Example:
 /// ```
 /// # use zjson::*;
 /// let (_, string) = parse_double_quoted_string("\"hello\"").unwrap();
 /// assert_eq!(string, Token::String("hello"));
 /// ```
 pub fn parse_double_quoted_string(input: &str) -> nom::IResult<&str, Token> {
-    context("string", delimited(tag("\""), tag("hello"), tag("\"")))(input)
-        .map(|(next_input, res)| (next_input, Token::String(res.into())))
+    context(
+        "double_quoted_string",
+        delimited(tag("\""), tag("hello"), tag("\"")),
+    )(input)
+    .map(|(next_input, res)| (next_input, Token::String(res.into())))
+}
+
+/// Parse a single or double quoted string.
+///
+/// ## Example:
+/// ```
+/// # use zjson::*;
+/// let (_, string) = parse_string("\"hello\"").unwrap();
+/// assert_eq!(string, Token::String("hello"));
+///
+/// let (_, string) = parse_string("'hello'").unwrap();
+/// assert_eq!(string, Token::String("hello"));
+/// ```
+pub fn parse_string(input: &str) -> nom::IResult<&str, Token> {
+    context(
+        "string",
+        alt((parse_single_quoted_string, parse_double_quoted_string)),
+    )(input)
 }
 
 #[cfg(test)]
@@ -38,7 +66,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn parse_expression() {
+    fn test_parse_single_quoted_string() {
         assert_eq!(
             parse_single_quoted_string("'hello'").unwrap().1,
             Token::String("hello")
@@ -46,5 +74,29 @@ mod tests {
         assert!(parse_single_quoted_string("'yoted").is_err());
         assert!(parse_single_quoted_string("yoted'").is_err());
         assert!(parse_single_quoted_string("yoted").is_err());
+    }
+
+    #[test]
+    fn test_parse_double_quoted_string() {
+        assert_eq!(
+            parse_double_quoted_string("\"hello\"").unwrap().1,
+            Token::String("hello")
+        );
+        assert!(parse_double_quoted_string("\"yoted").is_err());
+        assert!(parse_double_quoted_string("yoted\"").is_err());
+        assert!(parse_double_quoted_string("yoted").is_err());
+    }
+
+    #[test]
+    fn test_parse_string() {
+        assert_eq!(parse_string("\"hello\"").unwrap().1, Token::String("hello"));
+        assert!(parse_string("\"yoted'").is_err());
+        assert!(parse_string("'yoted\"").is_err());
+
+        assert_eq!(parse_string("'hello'").unwrap().1, Token::String("hello"));
+        assert!(parse_string("'yoted\"").is_err());
+        assert!(parse_string("\"yoted'").is_err());
+
+        assert!(parse_string("yoted").is_err());
     }
 }
